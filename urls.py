e2114e2dd2e6dcabcb2a53ce5ecbf8968459e452
Chris J. Karr <chris@audacious-software.com>
2021-10-30 21:06:49 +0000

    Adding placeholder urls.py file

---
+++ urls.py
@@ -0,0 +1,9 @@
+# pylint: disable=line-too-long
+
+# from django.conf.urls import url
+
+# from .views import cpu_load, current_users, disk_usage, total_processes, zombie_processes, background_jobs
+
+urlpatterns = [
+#    url(r'^cpu-load.json$', cpu_load, name='nagios_monitor_cpu_load'),
+]
