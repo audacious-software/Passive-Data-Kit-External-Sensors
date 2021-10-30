0b11613780f80a595bb1288f4e4d0476b311542d
Chris J. Karr <chris@audacious-software.com>
2021-10-30 20:50:02 +0000

    CircleCI integration

--- apps.py
+++ apps.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
-
 class PassiveDataKitExternalSensorsConfig(AppConfig):
     name = 'passive_data_kit_external_sensors'
+    verbose_name = 'Passive Data Kit: External Sensors'
+    default_auto_field = 'django.db.models.AutoField'
