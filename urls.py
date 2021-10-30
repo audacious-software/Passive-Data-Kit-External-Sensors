0438c2be90d7b4074409d46511f3b654cbc9bf9a
Chris J. Karr <chris@audacious-software.com>
2021-10-30 21:18:58 +0000

    Code cleanup

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@
 # from .views import cpu_load, current_users, disk_usage, total_processes, zombie_processes, background_jobs
 
 urlpatterns = [
-#    url(r'^cpu-load.json$', cpu_load, name='nagios_monitor_cpu_load'),
+    # url(r'^cpu-load.json$', cpu_load, name='nagios_monitor_cpu_load'),
 ]
