78f5fd45a7309fbb10687c9849cc9e573e517db1
Chris J. Karr <chris@audacious-software.com>
2021-10-30 20:45:04 +0000

    Initial empty app commit

---
+++ apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class PassiveDataKitExternalSensorsConfig(AppConfig):
+    name = 'passive_data_kit_external_sensors'
